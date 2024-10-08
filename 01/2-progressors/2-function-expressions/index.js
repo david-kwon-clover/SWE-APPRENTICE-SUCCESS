0779be6284eec1765f7c2e1bb6234583d07ed21d
David Kwon <david.kwon@clover.com>
2024-10-08 12:48:11 -0600

    Complete 01/2-progressors/2-function-expressions

--- 01/2-progressors/2-function-expressions/index.js
+++ 01/2-progressors/2-function-expressions/index.js
@@ -7,10 +7,9 @@
   strictlyEqual('billy', 'billy') // true
   strictlyEqual('billy', 'bill') // false
 */
-
-
-
-
+function strictlyEqual(val1, val2) {
+  return val1 === val2;
+}
 
 // 2. Define a function expression, `looselyEqual` that takes two parameters and checks if they are loosely equal
 /*
@@ -22,10 +21,9 @@
   looselyEqual('billy', 'bill') // false
   looselyEqual('false', false) // true
 */
-
-
-
-
+function looselyEqual(val1, val2) {
+  return val1 == val2;
+}
 
 // 3. Define a function expression, `isAdult` that takes in a number representing a person's age and returns true if they are 18 or older, and false otherwise.
 /*
@@ -35,10 +33,9 @@
   isAdult(17) // false
   isAdult(16) // false
 */
-
-
-
-
+function isAdult(age) {
+  return age >= 18 ? true : false;
+}
 
 // 4. Define a function expression, `isOdd` that takes in a number and returns true if it is odd, and false otherwise.
 /* 
@@ -49,6 +46,9 @@
   isOdd(4) // false
   // etc.
 */
+function isOdd(num) {
+  return num % 2 === 1 ? true : false;
+}
 
 // 5. Define a function expression, `isPositive` that takes in a number and returns true if it is positive, and false otherwise.
 /*
@@ -57,10 +57,9 @@
   isPositive(-1) // false
   isPositive(0) // false
 */
-
-
-
-
+function isPositive(num) {
+  return num > 0 ? true : false;
+}
 
 // 6. Define a function expression, `isLeapYear` that takes in a number representing a year and returns true if it is a leap year, and false otherwise. Hint: A leap year is divisible by 4, but not divisible by 100 unless also divisible by 400.
 /*
@@ -70,16 +69,11 @@
   isLeapYear(2100) // false
   isLeapYear(2400) // true
 */
+function isLeapYear(year) {
+  return (year % 4 === 0 && year % 100 !== 0) ||
+    (year % 4 === 0 && year % 400 === 0)
+    ? true
+    : false;
+}
 
-
-
-
-
-export { 
-  strictlyEqual, 
-  looselyEqual, 
-  isAdult, 
-  isOdd, 
-  isPositive, 
-  isLeapYear 
-};
+export { strictlyEqual, looselyEqual, isAdult, isOdd, isPositive, isLeapYear };
