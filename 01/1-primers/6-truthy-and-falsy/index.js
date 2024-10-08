f8afd899c8ad7cf896b15b8a0b31e709fc6561a0
David Kwon <david.kwon@clover.com>
2024-10-08 12:05:16 -0600

    Complete 01/1-primers/6-truthy-and-falsy

--- 01/1-primers/6-truthy-and-falsy/index.js
+++ 01/1-primers/6-truthy-and-falsy/index.js
@@ -1,7 +1,7 @@
 // 1. Two variables, truthy and falsy, have been created below. Assign the variable truthy to a truthy value and falsy to a falsy value.
 
-let truthy;
-let falsy;
+let truthy = 1;
+let falsy = 0;
 
 // 2. Create a function called truthyOrFalsy that accepts a value and returns a statement informing you if the value is truthy or falsy.
 /*
@@ -13,11 +13,17 @@ truthyOrFalsy("Hello World") ==> "This is a truthy value"
 */
 
 function truthyOrFalsy(val) {
-    // YOUR CODE
-
-
-
-
+  // YOUR CODE
+  if(val == false ||
+  val == undefined ||
+  val == null ||
+  val == "" ||
+  val == NaN ||
+  val == 0) {
+    return "This is a falsy value"
+  } else {
+    return "This is a truthy value"
+  }
 }
 
 // 3. Create a function called checkAge that accepts a number as an argument. Output the following based on the age provided: (a) if age is greater than or equal to 18 return "you are an adult", (b) if the age is less than 18, return "you are a minor", if the age is not provided return "Age is not provided".
@@ -28,12 +34,16 @@ checkAge(15) => "You are a minor"
 checkAge(null) => "Age is not provided"
 */
 function checkAge(age) {
-    // YOUR CODE
-
-
-
-
-    
+  // YOUR CODE
+  if(!age) {
+    return "Age is not provided";
+  } else {
+    if(age >= 18) {
+        return "You are an adult";
+    } else {
+        return "You are a minor";
+    }
+  }
 }
 
 // DO NOT EDIT BELOW
