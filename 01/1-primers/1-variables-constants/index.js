03255cb53c7f3a169890b6dfbef6f08d4d2c1045
David Kwon <david.kwon@clover.com>
2024-10-08 11:43:48 -0600

    Complete 01/1-primers/1-variables-constants

--- 01/1-primers/1-variables-constants/index.js
+++ 01/1-primers/1-variables-constants/index.js
@@ -1,22 +1,15 @@
 // 1. Define a new variable called `age` and set it equal to your age. Remember, it is a variable, so it should be able to be redefined.
-
-
-
+let age = 27; 
 
 // 2. Define a new constant called `name` and set it equal to your name.
 // Remember, it is a constant, so it should not be able to be redefined.
-
-
-
+const name = "David";
 
 // 3. Define a new variable called isACoder and set it to the boolean of true.
-
-
-
+const isACoder = true;
 
 // 4. Define a new constant called pi and set it equal to 3.1415
-
-
+const pi = 3.1415;
 
 
 // DO NOT EDIT BELOW
