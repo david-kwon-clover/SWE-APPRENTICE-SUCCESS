987a41b19ce675de7282b392cd55d9842368d32a
David Kwon <david.kwon@clover.com>
2024-10-08 12:06:55 -0600

    Complete 01/1-primers/7-functions

--- 01/1-primers/7-functions/index.js
+++ 01/1-primers/7-functions/index.js
@@ -1,13 +1,17 @@
 // 1. Create a new function declaration called `myName` that returns your name
-
-
+function myName() {
+    return "David";
+}
 
 // 2. Create a new function declaration called `currentYear` that returns the current year
-
-
+function currentYear() {
+    return 2024;
+}
 
 // 3. Create a new function called `favoriteNumber` that returns your favorite number
-
+function favoriteNumber() {
+    return 7;
+}
 
 // DO NOT EDIT BELOW
 export { myName, currentYear, favoriteNumber };
