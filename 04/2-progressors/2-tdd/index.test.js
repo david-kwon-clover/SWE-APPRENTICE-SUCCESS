c1b8510f4e7a27121e9ebe702712a5d16052e488
David Kwon <david.kwon@clover.com>
2024-10-17 10:53:19 -0600

    Complete tests for multiplyTwoIntegers function in 04/2-progressors/2-tdd

--- 04/2-progressors/2-tdd/index.test.js
+++ 04/2-progressors/2-tdd/index.test.js
@@ -3,7 +3,7 @@ import subtractTwoNumbers from "./subtractTwoNumbers.js";
 import multiplyTwoIntegers from "./multiplyTwoIntegers.js";
 import addingArrays from "./addingArrays.js";
 
-describe("subtractTwoNumbers", () => {
+describe("Subtracting and Multiplying", () => {
     let num1, num2, num3, num4;
     beforeEach(() => {
         num1 = 17;
@@ -12,7 +12,7 @@ describe("subtractTwoNumbers", () => {
         num4 = 71;
     })
 
-    test("should correctly subtract two numbers", () => {
+    test("subtractTwoNumbers should correctly subtract two numbers", () => {
         const test1 = subtractTwoNumbers(num1, num2);
         const test2 = subtractTwoNumbers(num2, num1);
         const test3 = subtractTwoNumbers(num4, num3);
@@ -21,6 +21,22 @@ describe("subtractTwoNumbers", () => {
         expect(test2).toEqual(-11);
         expect(test3).toEqual(33);
         expect(test4).toEqual(21);
+    })
+    
+    test("multiplyTwoIntegers should correctly mulitply two numbers", () => {
+        const test1 = multiplyTwoIntegers(num1, num2);
+        const test2 = multiplyTwoIntegers(num4, num3);
+        const test3 = multiplyTwoIntegers(num3, num1);
+        expect(test1).toEqual(102);
+        expect(test2).toEqual(2698);
+        expect(test3).toEqual(646);
+    })
+
+    test("multiplyTwoIntegers should detect non-valid arguments", () => {
+        const invalidInputs = multiplyTwoIntegers("14", "13");
+        const invalidInputs2 = multiplyTwoIntegers(1.1, 2.2);
+        expect(invalidInputs).toEqual("Inputs must be a number.");
+        expect(invalidInputs2).toEqual("Integers only.");
     })
 
 })
